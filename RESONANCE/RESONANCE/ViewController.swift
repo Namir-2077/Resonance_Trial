903c95fecf7bb4ca505753b82043e2200d7e54e2
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-11-14 09:35:47 +0530

    Main Push

---
+++ RESONANCE/RESONANCE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RESONANCE
+//
+//  Created by Student on 14/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RESONANCE/RESONANCETests/RESONANCETests.swift
@@ -0,0 +1,17 @@
+//
+//  RESONANCETests.swift
+//  RESONANCETests
+//
+//  Created by Student on 14/11/25.
+//
+
+import Testing
+@testable import RESONANCE
+
+struct RESONANCETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
